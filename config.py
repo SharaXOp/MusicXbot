b6ad10d8181071098cf36a5e31f0ca6d5d1283f0
MISHSINGH19 <141798577+MISHSINGH19@users.noreply.github.com>
2024-02-20 21:32:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,8 +32,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO",
-    "meri_ashhiqui",
+    "UPSTREAM_REPO", "https://github.com/MISHSINGH19/SHIV-V2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
